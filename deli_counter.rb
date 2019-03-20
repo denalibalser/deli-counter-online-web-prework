77276bde04d1f7a28d33e80e94186f89d19bebaf
Denali Balser <denali.balser167@gmail.com>
2019-03-20 15:32:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+  if array.length >= 1
+    new_array = []
+    counter = 1
+    array.each do |name|
+      new_array.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{new_array.join("")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+line(katz_deli)
